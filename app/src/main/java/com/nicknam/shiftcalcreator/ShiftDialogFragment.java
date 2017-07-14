68e93fe61f6f2f6db498af3334d1fc8408d3fa1f
S. Nicknam <s.nicknam@icoud.com>
2017-07-14 18:14:25 +0200

    Initial design of fragment shift dialog

---
+++ app/src/main/java/com/nicknam/shiftcalcreator/ShiftDialogFragment.java
@@ -0,0 +1,10 @@
+package com.nicknam.shiftcalcreator;
+
+import android.app.DialogFragment;
+
+/**
+ * Created by snick on 14-7-2017.
+ */
+
+public class ShiftDialogFragment extends DialogFragment {
+}
